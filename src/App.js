52254177030773599b7de921090e9d1206190d81
Nidhish <nidhish.ksrn@gmail.com>
2023-02-16 15:54:23 +0530

    added default component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { Routes, Route } from "react-router-dom";
 import Demo from './pages/demoApp';
 
 import DemoReview from './pages/demo-review';
+import Default from './pages/default';
+
 
 function App() {
   return <Routes>
 
-    <Route exact path="/" element={<Demo />} >   </Route>
+    <Route exact path="/" element={<Default />} >   </Route>
     <Route exact path="/demo-review" element={<DemoReview />} >
 
       
---
+++ src/pages/default.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+export default function Default() {
+
+   
+    return (
+
+        <div className="container">
+            <h3> Hello Hooks App</h3>
+
+            </div>
+    )
+}
